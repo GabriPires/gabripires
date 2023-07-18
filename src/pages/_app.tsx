1f55dcf61a69d39bed85f8a977897bed2f22a08e
Gabriel Pires <gabpiresdev@gmail.com>
2023-07-18 16:14:00 -0300

    feat: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
+import { NextSeo } from 'next-seo'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextSeo
+        title="Gabriel Pires | Frontend Developer"
+        description="Este é meu portfólio! Fique a vontade para explorar todos os meus projetos e entrar em contato comigo."
+        // canonical="https://gabrielpires.dev.br"
+        openGraph={{
+          // url: 'https://gabrielpires.dev.br',
+          title: 'Gabriel Pires | Frontend Developer',
+          description: 'Portfólio do Gabriel Pires',
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
